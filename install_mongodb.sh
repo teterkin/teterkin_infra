c5ae9b0e0b4981042b21905fa02d9a28b201f674
Teterkin Alexander <teterkin@hostco.ru>
2020-12-07 22:45:49 +0500

    Fix: Add apt-transport-https package

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -6,6 +6,7 @@ echo Installing and Starting MongoDB
 echo -------------------------------
 
 sudo rm /etc/apt/sources.list.d/mongodb*.list
+sudo apt install apt-transport-https
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927 && \
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list && \
 sudo apt update && \
